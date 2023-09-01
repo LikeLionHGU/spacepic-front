4632c64a3e4c733417a8b7ae5cadd36d419f6025
healim01 <22000770@handong.edu>
2023-09-01 16:15:08 +0900

    :lipstick: style: add theme

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    color: {
+      main: '#3964c3',
+      mint: '#7ed8b8',
+      darkblue: '#2e4b90',
+    },
+    mono: {
+      black: '#1A1C19',
+      gray1: '#1A1C19',
+      gray2: '#8D948C',
+      gray3: '#CFD1D5',
+      gray4: '#F1F2F1',
+      white: '#FFFFFF',
+    },
+  },
+});
+
+export default theme;
