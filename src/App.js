2277a2ff934fff085ba1b6850bb0920832575104
healim01 <22000770@handong.edu>
2023-09-01 16:15:32 +0900

    :sparkles: feat: add packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { ThemeProvider } from '@mui/material/styles';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Router from './Router';
+import theme from './theme';
+import { RecoilRoot } from 'recoil';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <ThemeProvider theme={theme}>
+            {/* <GoogleOAuthProvider
+              clientId={process.env.REACT_APP_CLIENT_ID as any}
+            > */}
+            <Router />
+            <ReactQueryDevtools initialIsOpen={true} />
+            {/* </GoogleOAuthProvider> */}
+          </ThemeProvider>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
