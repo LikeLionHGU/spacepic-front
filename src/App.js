56e151e26301b6394dd80c714f20d45ce28fd3ae
healim01 <22000770@handong.edu>
2023-09-02 20:21:41 +0900

    :lipstick: style: add global style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,35 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import Router from './Router';
 import theme from './theme';
 import { RecoilRoot } from 'recoil';
+import { createGlobalStyle } from 'styled-components';
 // import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap');
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+*[hidden] {
+    display: none;
+}
+`;
 
 const queryClient = new QueryClient();
 
@@ -17,6 +45,7 @@ function App() {
             {/* <GoogleOAuthProvider
               clientId={process.env.REACT_APP_CLIENT_ID as any}
             > */}
+            <GlobalStyle />
             <Router />
             <ReactQueryDevtools initialIsOpen={true} />
             {/* </GoogleOAuthProvider> */}
