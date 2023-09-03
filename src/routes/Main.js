de1d3ac32e33eb43d96b66dbe109531774ae9d74
healim01 <22000770@handong.edu>
2023-09-03 22:51:37 +0900

    :sparkles: feat: img layout

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -25,37 +25,52 @@ const PhotoArray = [
   },
   {
     id: 5,
-    desc: '몰라요! 하지만 아름다우시네요!',
-    photoUrl:
-      'https://postfiles.pstatic.net/MjAyMjAyMDZfMTM3/MDAxNjQ0MTA5NTAwNDE1.yK-HfolvLk80qaQYJohM86lF7gSSHS8XpTj0pBqpPT0g.BQK2eQnPU-sXxi6_Th6Vt8YHde35xns_HVI3Y5kC2xUg.JPEG.anknara/f4cc1c3cb6375f9e3263f3de372c69e6.jpg?type=w773',
+    desc: '김세정씨와 김나영씨 그리고 구구단',
+    photoUrl: 'https://i.pinimg.com/736x/3f/0b/c4/3f0bc45edd547370e6aff78ef949b833.jpg',
   },
   {
     id: 6,
-    desc: '트와이스 ',
-    photoUrl:
-      'https://postfiles.pstatic.net/MjAyMjAyMTBfNTYg/MDAxNjQ0NDIwMjE0NTk4.W_ECc2TTwo35JQGgKlR5bFcdcJOqhrjIszdetSW0vi4g.GFYhCYawiBjnvFJ8Rewy_nJ9jh3A3JtQhn3nUBywsoYg.JPEG.anknara/a57c81d238ac9db99d65a6bf0ae3f422.jpg?type=w773',
+    desc: '에이프릴 나은',
+    photoUrl: 'https://i.pinimg.com/736x/40/62/1e/40621edbace8925c63ef4757e8f48f59.jpg',
   },
   {
     id: 7,
-    desc: '귀여우시네요 당신!',
-    photoUrl:
-      'https://postfiles.pstatic.net/MjAyMTEyMzBfODcg/MDAxNjQwNzkyNTkzMDE1.8gCgR-7e8VeLOCilAee6bP7hYyQoB-rtzn093bEL41Ig.XNyuy3DWGEk0NBbWQoKN6tOqTnduICxLvxo_XuZwtFIg.JPEG.anknara/E7JAyeYVkAcq8ut.jpeg.jpg?type=w773',
+    desc: '에이프릴 나은22',
+    photoUrl: 'https://i.pinimg.com/736x/a2/6e/c2/a26ec20255eb3233ab4949902674e7c7.jpg',
   },
   {
     id: 8,
-    desc: '또다른 엔믹스! 이쁘시네요!',
+    desc: '엔믹스 누군지 모르는 분과 혜원',
     photoUrl:
-      'https://postfiles.pstatic.net/MjAyMjA4MDNfMTA3/MDAxNjU5NTEyNjg3NzM5.8U8yL9oFFmKMg_OX9PXmNMCL57VQeO-nYqoA2p5capEg.bl0X5e77ifWECs9GGumRy2i5fC4Gz6LGj_stxmPI_pkg.JPEG.anknara/f95794e537f591f4f1df9282b7ce5eb9.jpg?type=w773',
+      'https://mblogthumb-phinf.pstatic.net/MjAyMjA4MDNfODkg/MDAxNjU5NTEyNjYzNzMw.J6v8-zDCkkIgZirM1vDBWPMTPyk8ZHCvic6T51NNW6Qg.Ee4l6XSm4CLTf3B-EOwZhBCRysJKHVtDCTUdzJSC0yQg.JPEG.anknara/ffb5940a4b36502b2745856dcfe7c0f7.jpg?type=w800',
   },
 ];
 
 const Page = styled.div`
-  margin: 50px 200px;
+  margin: 50px 250px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const PhotoFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  margin: 20px;
+  /* display: grid;
+  grid-template-columns: repeat(4, 1fr); */
+  /* grid-auto-rows: 1fr; */
 `;
 
 const Photo = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  /* width: 275px; */
+  /* width: calc(25% - 20px); */
+  /* height: auto; */
+  /* height: ${props => (props.num % 2 === 1 ? '500px' : '600px')}; */
+  /* background-color: ${props => (props.num % 2 === 1 ? 'red' : 'blue')}; */
+  margin: 10px;
+  box-sizing: border-box;
 `;
 
 const Img = styled.img`
@@ -66,22 +81,43 @@ const Img = styled.img`
 const Context = styled.div`
   border: 1px solid black;
   width: 275px;
-  height: ${props => props.size || '200px'};
+  height: ${props => props.size || '200px '};
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+const preparedData = {
+  0: [],
+  1: [],
+  2: [],
+  3: [],
+};
+const prepare = () => {
+  var i = 0;
+  PhotoArray.forEach(photo => {
+    preparedData[i % 4].push(photo);
+    i++;
+  });
+};
 
 export default function Main() {
+  prepare();
+  const dataArrays = [preparedData[0], preparedData[1], preparedData[2], preparedData[3]];
+
   return (
     <>
       <Header />
       <Page>
-        <Photo>
-          {PhotoArray.map(photo => (
-            <div key={photo.id}>
-              <Img src={photo.photoUrl} alt={photo.desc} />
-              {photo.id % 2 === 1 ? <Context size="125px"> 홀수 </Context> : <Context> 짝수 </Context>}
-            </div>
-          ))}
-        </Photo>
+        {dataArrays.map((dataArray, index) => (
+          <PhotoFrame key={index}>
+            {dataArray.map(photo => (
+              <Photo key={photo.id} num={photo.id}>
+                <Img src={photo.photoUrl} alt={photo.desc} />
+                {photo.id % 2 === 1 ? <Context size="125px"> 홀수 </Context> : <Context> 짝수 </Context>}
+              </Photo>
+            ))}
+          </PhotoFrame>
+        ))}
       </Page>
     </>
   );
