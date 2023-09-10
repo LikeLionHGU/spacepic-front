e976d85db9e6cdfcf0b9aaaf942e98b140ddc801
healim01 <22000770@handong.edu>
2023-09-10 16:46:04 +0900

    feat: new home page

---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import FirstPage from '../components/FirstPage';
+import GalleryView from '../components/GalleryView';
+import MiddleSlide from '../components/MiddleSlide';
+
+export default function Home() {
+  return (
+    <>
+      <FirstPage />
+      <MiddleSlide />
+      <GalleryView />
+    </>
+  );
+}
