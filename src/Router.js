077e60f599080e7dd073f4ee2f3a4c3f189046d7
healim01 <22000770@handong.edu>
2023-09-10 16:45:54 +0900

    feat: new route

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,25 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './routes/Main';
 import AddPhoto from './routes/AddPhoto';
+import Apple from './routes/AppleTest';
+import Home from './routes/Home';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Switch>
         <>
-          <Route exact path="/">
+          <Route exact path="/main">
             <Main />
+          </Route>
+          <Route exact path="/">
+            <Home />
           </Route>
           <Route exact path="/addphoto">
             <AddPhoto />
+          </Route>
+          <Route exact path="/apple">
+            <Apple />
           </Route>
         </>
       </Switch>
