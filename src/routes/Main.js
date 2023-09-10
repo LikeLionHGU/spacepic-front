a14d7235c8fc5eb557350b3252c512c3f4fe67e3
healim01 <22000770@handong.edu>
2023-09-10 15:04:18 +0900

    feat: change context

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -23,26 +23,27 @@ const PhotoArray = [
     photoUrl:
       'https://mblogthumb-phinf.pstatic.net/MjAyMjA4MDNfODkg/MDAxNjU5NTEyNjYzNzMw.J6v8-zDCkkIgZirM1vDBWPMTPyk8ZHCvic6T51NNW6Qg.Ee4l6XSm4CLTf3B-EOwZhBCRysJKHVtDCTUdzJSC0yQg.JPEG.anknara/ffb5940a4b36502b2745856dcfe7c0f7.jpg?type=w800',
   },
+
   {
     id: 5,
-    desc: '김세정씨와 김나영씨 그리고 구구단',
-    photoUrl: 'https://i.pinimg.com/736x/3f/0b/c4/3f0bc45edd547370e6aff78ef949b833.jpg',
-  },
-  {
-    id: 6,
     desc: '에이프릴 나은',
     photoUrl: 'https://i.pinimg.com/736x/40/62/1e/40621edbace8925c63ef4757e8f48f59.jpg',
   },
   {
-    id: 7,
-    desc: '에이프릴 나은22',
-    photoUrl: 'https://i.pinimg.com/736x/a2/6e/c2/a26ec20255eb3233ab4949902674e7c7.jpg',
+    id: 6,
+    desc: '김세정씨와 김나영씨 그리고 구구단',
+    photoUrl: 'https://i.pinimg.com/736x/3f/0b/c4/3f0bc45edd547370e6aff78ef949b833.jpg',
   },
   {
-    id: 8,
+    id: 7,
     desc: '엔믹스 누군지 모르는 분과 혜원',
     photoUrl:
       'https://mblogthumb-phinf.pstatic.net/MjAyMjA4MDNfODkg/MDAxNjU5NTEyNjYzNzMw.J6v8-zDCkkIgZirM1vDBWPMTPyk8ZHCvic6T51NNW6Qg.Ee4l6XSm4CLTf3B-EOwZhBCRysJKHVtDCTUdzJSC0yQg.JPEG.anknara/ffb5940a4b36502b2745856dcfe7c0f7.jpg?type=w800',
+  },
+  {
+    id: 8,
+    desc: '에이프릴 나은22',
+    photoUrl: 'https://i.pinimg.com/736x/a2/6e/c2/a26ec20255eb3233ab4949902674e7c7.jpg',
   },
 ];
 
@@ -113,7 +114,11 @@ export default function Main() {
             {dataArray.map(photo => (
               <Photo key={photo.id} num={photo.id}>
                 <Img src={photo.photoUrl} alt={photo.desc} />
-                {photo.id % 2 === 1 ? <Context size="125px"> 홀수 </Context> : <Context> 짝수 </Context>}
+                {photo.id % 2 === 1 ? (
+                  <Context size="125px"> 홀수 / {photo.desc} </Context>
+                ) : (
+                  <Context> 짝수 / {photo.desc} </Context>
+                )}
               </Photo>
             ))}
           </PhotoFrame>
