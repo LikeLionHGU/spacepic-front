45537158a4794a929e9ff83759d4299d8dfe1a87
healim01 <22000770@handong.edu>
2023-09-12 22:04:13 +0900

    style: add gradient

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,17 @@ import styled from 'styled-components';
 const Div = styled.div`
   width: 100vw;
   height: 30vh;
-  background-color: black;
+  background: linear-gradient(to top, #a15ef6, black); /* 보라색 그라데이션 */
+  color: white; /* 글자 색을 흰색으로 설정 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default function Footer() {
   return (
     <>
-      <Div>Footer</Div>
+      <Div> </Div>
     </>
   );
 }
