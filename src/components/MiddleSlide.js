b0bac4d2b49b08f82a77164cf6427b06890952cd
healim01 <22000770@handong.edu>
2023-09-12 22:03:07 +0900

    style: change font size

--- src/components/MiddleSlide.js
+++ src/components/MiddleSlide.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Page = styled.div`
   height: 50vh;
   width: 100vw;
-  background-color: gray;
+  /* background-color: gray; */
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -13,15 +13,18 @@ const Page = styled.div`
 
 const Desc = styled.div`
   margin: 5px;
-  font-size: 22px;
+  font-size: 16px;
 `;
 
 export default function MiddleSlide() {
   return (
     <>
       <Page>
+        <Desc> Spacepic에서 당신의 행성을 공유하세요! </Desc>
+        <div style={{ height: '50px' }} />
         <Desc> 우리는 이러쿵저러쿵할 예정이며,</Desc>
-        <Desc>그로인해 유저는 이러한 장점을 누릴 수 있습니다!</Desc>
+        <Desc>그로인해 유저는 이러한</Desc>
+        <Desc> 장점을 누릴 수 있습니다!</Desc>
         <Desc>들어가보실까용? </Desc>
       </Page>
     </>
