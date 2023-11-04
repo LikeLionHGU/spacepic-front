581d29e1093aad851e047e447f2316f76245161a
Hyelim Choi <HyelimChoi01@gmail.com>
2023-11-04 19:32:17 +0900

    feat: dummy api 연결

--- src/apis/photoApi.js
+++ src/apis/photoApi.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
+export const GetDummyPhotos = async () => {
+  const response = await axios.get(
+    `${process.env.REACT_APP_BASE_URL}/api/get/sample`
+  );
+  return response;
+};
+
 export const GetPhotos = async (memberId) => {
   const response = await axios.get(
     `${process.env.REACT_APP_BASE_URL}/api/post/get/member/${memberId}`
