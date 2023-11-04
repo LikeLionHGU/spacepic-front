ee03e5d48e95d955ad4a810a0e17618249a7f992
Hyelim Choi <HyelimChoi01@gmail.com>
2023-11-04 19:11:26 +0900

    feat: archive

--- src/apis/photoApi.js
+++ src/apis/photoApi.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export const GetPhotos = async () => {
-  const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/post/get`);
+export const GetPhotos = async (memberId) => {
+  const response = await axios.get(
+    `${process.env.REACT_APP_BASE_URL}/api/post/get/member/${memberId}`
+  );
   return response;
 };
