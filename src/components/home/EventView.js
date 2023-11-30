9b70a49c216849140f9eb81ef06a63384451cdd8
Hyelim Choi <HyelimChoi01@gmail.com>
2023-11-30 17:05:15 +0900

    feat: add photo btn

--- src/components/home/EventView.js
+++ src/components/home/EventView.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { ReactComponent as MainDesc } from '../../assets/imgs/LogoW.icon.svg';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Container = styled.div`
   display: flex;
@@ -21,6 +22,21 @@ const Event = styled.div`
   margin: 30px 0px;
 `;
 
+const AddPhotoButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 77px;
+  height: 36px;
+  background-color: #494949;
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
+  cursor: pointer;
+  border: none;
+  outline: none;
+`;
+
 export default function EventView() {
   return (
     <Container>
@@ -29,6 +45,10 @@ export default function EventView() {
         <div>이번주 컨셉 주제: </div>
         크리스마스와 종강을 기다리는 여러분을 표현해주세요!
       </Event>
+
+      <Link to="/addphoto" underline="none">
+        <AddPhotoButton>개시하기</AddPhotoButton>
+      </Link>
 
       {/* TODO: 랭크 순위 배치 */}
 
