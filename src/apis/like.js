cf5e41b8632a7f6545b0faf864de13a94272c902
Hyelim Choi <HyelimChoi01@gmail.com>
2023-12-06 03:35:39 +0900

    chore: combine apis

--- src/apis/like.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from '../axios';
-
-export const likePhoto = async (postId, memberId) => {
-  const response = await axiosInstance.post('/post/like', {
-    memberId,
-    postId,
-  });
-  return response;
-};
--- src/apis/photoApi.js
+++ src/apis/photoApi.js
@@ -9,3 +9,11 @@ export const GetPhotos = async (memberId) => {
   const response = await axiosInstance.get(`/api/post/get/member/${memberId}`);
   return response;
 };
+
+export const likePhoto = async (postId, memberId) => {
+  const response = await axiosInstance.post('/post/like', {
+    memberId,
+    postId,
+  });
+  return response;
+};
