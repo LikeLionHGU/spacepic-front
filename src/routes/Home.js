12c5af20999975a3bd96ad022f140f8f24c60b16
Hyelim Choi <HyelimChoi01@gmail.com>
2023-12-05 12:34:11 +0900

    delete: rm footer

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet';
 
-import Footer from '../components/Footer';
+// import Footer from '../components/Footer';
 import Header from '../components/Header';
 // import MainGrid from '../components/MainGrid';
 import EventView from '../components/home/EventView';
@@ -13,11 +13,8 @@ export default function Home() {
       </Helmet>
 
       <Header currMenu="Home" />
-
       <EventView />
-      {/* <MainGrid /> */}
-
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
